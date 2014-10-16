b29e326c573020e6dfae6c837eefd210591f4bee
jeslipp <jeslipp@gmail.com>
2014-10-15 20:22:16 -0700

    changed loading

--- server.R
+++ server.R
@@ -3,7 +3,7 @@ library(shiny)
 library(dplyr)
 
 # load data
-readRDS("./data/data_clean.RData")
+load("data_clean.RData")
 
 # Define server logic required to summarize and view the selected
 # dataset
