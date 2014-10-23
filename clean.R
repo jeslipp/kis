17cf9c3442478ac3b84ff429f83beb230dc7af34
jeslipp <jeslipp@gmail.com>
2014-10-22 17:06:31 -0700

    added gini index plus other smaller fixes

---
+++ clean.R
@@ -0,0 +1,150 @@
+library(stringr)
+library(reshape2)
+library(tidyr)
+library(dplyr)
+#library(ineq)
+
+### Anastassiadis 2011 data
+# read data
+# all at 0.5uM
+path <- "~/home/github/local/kis2/data/anastassiadis2011.csv"
+raw <- read.csv(path, 
+                skip = 1, 
+                stringsAsFactors = FALSE, 
+                check.names = TRUE, 
+                header = FALSE)
+
+# clean data
+data <- raw[3:302, 1:179]
+inhibitor_names <- raw[1, 1:179]
+inhibitor_names <- str_replace_all(unlist(inhibitor_names), "[ ]+", " ")
+inhibitor_names <- str_replace_all(unlist(inhibitor_names), "\x9a", "o")
+#inhibitors <- raw[2, 1:179]
+colnames(data) <- paste(inhibitor_names, "0.5", sep = "@")
+colnames(data)[1] <- "kinase"
+
+
+# convert data to long format
+data <- melt(data, 
+             id.vars = "kinase", 
+             variable.name = "compound", 
+             value.name = "percent_activity")
+data$compound <- as.character(data$compound)
+data$percent_activity <- as.integer(data$percent_activity)
+data$percent_activity <- ifelse(data$percent_activity < 0, 0, ifelse(data$percent_activity > 100, 100, data$percent_activity))
+#data$percent_activity <- as.numeric(data$percent_activity)
+data <- data %>%
+  group_by(compound) %>%
+  mutate(gini_compound = round(ineq(100 - percent_activity, type = "Gini"), 3)) %>%
+  group_by(kinase) %>%
+  mutate(gini_kinase = round(ineq(100 - percent_activity, type = "Gini"), 3))
+  
+data <- data[complete.cases(data), ]
+data <- select(data, kinase, compound, percent_activity, gini_kinase, gini_compound)
+#data <- select(data, kinase, compound, percent_activity)
+anastassiadis2011 <- data.frame(data)
+save(anastassiadis2011, file = "~/home/github/local/kis2/anastassiadis2011.RData")
+
+
+### Gao 2013 data
+
+# read data
+path <- "~/home/github/local/kis2/data/gao2013.csv"
+raw <- read.csv(path, 
+                skip = 1, 
+                stringsAsFactors = FALSE, 
+                check.names = TRUE, 
+                header = FALSE)
+
+# clean data
+data <- raw[2:235, ]
+inhibitor_names <- unlist(raw[1, -1])
+Encoding(inhibitor_names) <- "UTF-8"
+inhibitor_names <- str_split(inhibitor_names, " @ ")
+inhibitors <- sapply(inhibitor_names, "[", 1)
+inhibitors <- str_replace(inhibitors, "\x9a", "o")
+concentration <- sapply(inhibitor_names, "[", 2)
+concentration <- str_extract(concentration, "^[10]{1,2}")
+inhibitors <- paste(inhibitors, concentration, sep = "@")
+inhibitors <- c("kinase", inhibitors)
+colnames(data) <- inhibitors
+
+data <- data %>%
+  melt(id.vars = "kinase", variable.name = "compound", value.name = "percent_activity")
+
+data$compound <- as.character(data$compound)
+data$percent_activity <- as.integer(data$percent_activity)
+data$percent_activity <- ifelse(data$percent_activity < 0, 0, ifelse(data$percent_activity > 100, 100, data$percent_activity))
+# 
+data <- data %>%
+  group_by(compound) %>%
+  mutate(gini_compound = round(ineq(100 - percent_activity, type = "Gini"), 3)) %>%
+  group_by(kinase) %>%
+  mutate(gini_kinase = round(ineq(100 - percent_activity, type = "Gini"), 3))
+
+data <- select(data, kinase, compound, percent_activity, gini_kinase, gini_compound)
+#data <- select(data, kinase, compound, percent_activity)
+data <- data[complete.cases(data), ]
+gao2013 <- data.frame(data)
+save(gao2013, file = "~/home/github/local/kis2/gao2013.RData")
+
+
+### Kinase screen mrc data
+
+path_screen <- "~/home/github/local/kis2/data/kinase_inhibitor_results_2014-10-18T04-26-51.csv"
+raw <- read.csv(path_screen, stringsAsFactors = FALSE)
+data <- raw %>% 
+  select(Kinase, CNumber, Screen.Conc, Inhibition) %>%
+  rename(kinase = Kinase, 
+         compound = CNumber, 
+         concentration = Screen.Conc, 
+         percent_activity = Inhibition) %>%
+  arrange(kinase)
+#names(data) <- c("kinase", "compound", "concentration", "percent_activity")
+
+path_names <- "~/home/github/local/kis2/data/kinase_inhibitor_list_2014-10-18T04-00-06.csv"
+inhibitor_names <- read.csv(path_names, stringsAsFactors = FALSE)
+inhibitors <- inhibitor_names$Inhibitor
+names(inhibitors) <- inhibitor_names$CNumber
+
+data$compound <- inhibitors[as.character(data$compound)]
+#data$compound <- as.character(data$compound)
+data$concentration <- str_replace(data$concentration, " ", "")
+data$concentration <- as.numeric(data$concentration)
+
+data <- data %>%
+  group_by(kinase, compound, concentration) %>%
+  summarise(percent_activity = mean(percent_activity)) %>%
+  ungroup()
+
+data$percent_activity <- as.integer(data$percent_activity)
+data$percent_activity <- ifelse(data$percent_activity < 0, 0, ifelse(data$percent_activity > 100, 100, data$percent_activity))
+
+data$concentration <- as.character(data$concentration)
+data$concentration[is.na(data$concentration)] <- "unknown"
+data <- data %>%
+  unite(inhibitor, compound, concentration, sep = "@") %>%
+  rename(compound = inhibitor) %>%
+  arrange(kinase)
+
+#data$compound <- as.character(data$compound)
+#data$percent_activity <- as.integer(data$percent_activity)
+
+
+
+data <- data %>%
+  group_by(compound) %>%
+  mutate(gini_compound = round(ineq(100 - percent_activity, type = "Gini"), 3)) %>%
+  group_by(kinase) %>%
+  mutate(gini_kinase = round(ineq(100 - percent_activity, type = "Gini"), 3))
+
+data <- select(data, kinase, compound, percent_activity, gini_kinase, gini_compound)
+#data <- select(data, kinase, compound, percent_activity)
+
+#data <- data.frame(data)
+# compound 299 is duplicated
+# data <- data[!duplicated(data), ]
+data <- data[complete.cases(data), ]
+
+kid2014 <- data.frame(data)
+save(kid2014, file = "~/home/github/local/kis2/kid2014.RData")
--- global.R
+++ global.R
@@ -9,81 +9,170 @@ load("gao2013.RData")
 load("kid2014.RData")
 
 queryKinase <- function(input, data) {
-  offtarget_rate <- data %>%
+  
+  # calculate offtarget rate of compounds at given cut-off activity
+  percent_offtarget <- data %>%
     group_by(compound) %>%
-    summarise(percent_offtarget = round(sum(percent_activity <= input$query_cut, na.rm = TRUE) / n() * 100, 0))
-    
+    summarise(percent_offtarget = sum(percent_activity <= input$query_cut, na.rm = TRUE)) %>%
+    mutate(percent_offtarget = round(percent_offtarget / n() * 100, 0))
+  
+  # determine activity of specified offtarget kinases at given exclusion cut-off activity
   if (is.null(input$exclusion)) {
     query <- data %>%
+      select(-gini_kinase) %>%
       filter(kinase == input$query & percent_activity <= input$query_cut) %>%
-      select(compound, kinase, percent_activity) %>%
-      left_join(offtarget_rate, by = "compound") %>%
-      arrange(percent_activity)
-    
+      inner_join(percent_offtarget, by = "compound") %>%
+      arrange(percent_activity) %>%
+      rename(Inhibitor = compound, 
+             Kinase = kinase, 
+             "Activity (%)" = percent_activity, 
+             "Gini (Inhibitor)" = gini_compound, 
+             "Off-target (% Kinome)" = percent_offtarget)
   } else {
     offtarget <- data %>%
       filter(kinase %in% input$exclusion) %>%
-      dcast(compound ~ kinase, value.var = "percent_activity", fun.aggregate = mean)
+      group_by(compound) %>%
+      filter(min(percent_activity) >= input$exclusion_cut) %>%
+      select(kinase, compound, percent_activity) %>%
+      dcast(compound ~ kinase, value.var = "percent_activity")
     
+    # determine compounds that inhibit query kinase at given cut-off activity
     query <- data %>%
+      #select(-gini_kinase) %>%
       filter(kinase == input$query & percent_activity <= input$query_cut) %>%
-      left_join(offtarget_rate, by = "compound") %>%
-      left_join(offtarget, by = "compound")
-    
-    query$offtarget <- apply(select(query, -c(compound, kinase, percent_activity, percent_offtarget)), 1, min, na.rm = TRUE)
-    
-    query <- query %>% 
-      filter(offtarget > input$exclusion_cut) %>%
-      select(-offtarget) %>%
-      arrange(percent_activity) 
+      inner_join(percent_offtarget, by = "compound") %>%
+      inner_join(offtarget, by = "compound") %>%
+      arrange(percent_activity) %>%
+      rename(Inhibitor = compound, 
+             Kinase = kinase, 
+             "Activity (%)" = percent_activity, 
+             "Gini (Inhibitor)" = gini_compound, 
+             "Off-target (% Kinome)" = percent_offtarget)
   }
-  # return table
-  if (nrow(query) == 0) {
-    query <- data.frame("compound" = "N/A", 
-                        "kinase" = "N/A", 
-                        "percent_activity" = "N/A", 
-                        "percent_offtarget" = "N/A")
-  } else {
-    query
-  }
+  
+  query
 }
 
 queryInhibitor <- function(input, data) {
-  offtarget_rate <- data %>%
+  # calculate offtarget rate of kinases at given cut-off activity
+  percent_offtarget <- data %>%
     group_by(kinase) %>%
-    summarise(percent_offtarget = round(sum(percent_activity <= input$query_cut, na.rm = TRUE) / n() * 100, 0))
+    summarise(percent_offtarget = sum(percent_activity <= input$query_cut, na.rm = TRUE)) %>%
+    mutate(percent_offtarget = round(percent_offtarget / n() * 100, 0))
   
+  # determine activity of specified offtarget compounds at given exclusion cut-off activity
   if (is.null(input$exclusion)) {
     query <- data %>%
+      select(-gini_compound) %>%
       filter(compound == input$query & percent_activity <= input$query_cut) %>%
-      select(kinase, compound, percent_activity) %>%
-      left_join(offtarget_rate, by = "kinase") %>%
-      arrange(percent_activity)
+      inner_join(percent_offtarget, by = "kinase") %>%
+      arrange(percent_activity) %>%
+      rename(Inhibitor = compound, 
+             Kinase = kinase, 
+             "Activity (%)" = percent_activity, 
+             "Gini (Kinase)" = gini_kinase, 
+             "Off-target (% Kinome)" = percent_offtarget)
   } else {
     offtarget <- data %>%
       filter(compound %in% input$exclusion) %>%
-      dcast(kinase ~ compound, value.var = "percent_activity", fun.aggregate = mean)
+      group_by(kinase) %>%
+      filter(min(percent_activity) >= input$exclusion_cut) %>%
+      select(kinase, compound, percent_activity) %>%
+      dcast(kinase ~ compound, value.var = "percent_activity")
     
+    # determine compounds that inhibit query kinase at given cut-off activity
     query <- data %>%
+      select(-gini_compound) %>%
       filter(compound == input$query & percent_activity <= input$query_cut) %>%
-      left_join(offtarget_rate, by = "kinase") %>%
-      left_join(offtarget, by = "kinase")
-    
-    query$offtarget <- apply(select(query, -c(compound, kinase, percent_activity, percent_offtarget)), 1, min)
-    
-    query <- query %>% 
-      filter(offtarget > input$exclusion_cut) %>%
-      select(-offtarget) %>%
-      arrange(percent_activity) 
+      inner_join(percent_offtarget, by = "kinase") %>%
+      inner_join(offtarget, by = "kinase") %>%
+      arrange(percent_activity) %>%
+      rename(Inhibitor = compound, 
+             Kinase = kinase, 
+             "Activity (%)" = percent_activity, 
+             "Gini (Kinase)" = gini_kinase, 
+             "Off-target (% Kinome)" = percent_offtarget)
   }
-  # return table
-  if (nrow(query) == 0) {
-    query <- data.frame("compound" = "N/A", 
-                        "kinase" = "N/A", 
-                        "percent_activity" = "N/A", 
-                        "percent_offtarget" = "N/A")
-  } else {
-    query
-  }
-
+  
+  query
 }
+
+# queryKinase <- function(input, data) {
+#   offtarget_rate <- data %>%
+#     group_by(compound) %>%
+#     summarise(percent_offtarget = round(sum(percent_activity <= input$query_cut, na.rm = TRUE) / n() * 100, 0))
+#     
+#   if (is.null(input$exclusion)) {
+#     query <- data %>%
+#       filter(kinase == input$query & percent_activity <= input$query_cut) %>%
+#       select(compound, kinase, percent_activity) %>%
+#       left_join(offtarget_rate, by = "compound") %>%
+#       arrange(percent_activity)
+#     
+#   } else {
+#     offtarget <- data %>%
+#       filter(kinase %in% input$exclusion) %>%
+#       dcast(compound ~ kinase, value.var = "percent_activity", fun.aggregate = mean)
+#     
+#     query <- data %>%
+#       filter(kinase == input$query & percent_activity <= input$query_cut) %>%
+#       left_join(offtarget_rate, by = "compound") %>%
+#       left_join(offtarget, by = "compound")
+#     
+#     query$offtarget <- apply(select(query, -c(compound, kinase, percent_activity, percent_offtarget)), 1, min, na.rm = TRUE)
+#     
+#     query <- query %>% 
+#       filter(offtarget > input$exclusion_cut) %>%
+#       select(-offtarget) %>%
+#       arrange(percent_activity) 
+#   }
+#   # return table
+#   if (nrow(query) == 0) {
+#     query <- data.frame("compound" = "N/A", 
+#                         "kinase" = "N/A", 
+#                         "percent_activity" = "N/A", 
+#                         "percent_offtarget" = "N/A")
+#   } else {
+#     query
+#   }
+# }
+# 
+# queryInhibitor <- function(input, data) {
+#   offtarget_rate <- data %>%
+#     group_by(kinase) %>%
+#     summarise(percent_offtarget = round(sum(percent_activity <= input$query_cut, na.rm = TRUE) / n() * 100, 0))
+#   
+#   if (is.null(input$exclusion)) {
+#     query <- data %>%
+#       filter(compound == input$query & percent_activity <= input$query_cut) %>%
+#       select(kinase, compound, percent_activity) %>%
+#       left_join(offtarget_rate, by = "kinase") %>%
+#       arrange(percent_activity)
+#   } else {
+#     offtarget <- data %>%
+#       filter(compound %in% input$exclusion) %>%
+#       dcast(kinase ~ compound, value.var = "percent_activity", fun.aggregate = mean)
+#     
+#     query <- data %>%
+#       filter(compound == input$query & percent_activity <= input$query_cut) %>%
+#       left_join(offtarget_rate, by = "kinase") %>%
+#       left_join(offtarget, by = "kinase")
+#     
+#     query$offtarget <- apply(select(query, -c(compound, kinase, percent_activity, percent_offtarget)), 1, min)
+#     
+#     query <- query %>% 
+#       filter(offtarget > input$exclusion_cut) %>%
+#       select(-offtarget) %>%
+#       arrange(percent_activity) 
+#   }
+#   # return table
+#   if (nrow(query) == 0) {
+#     query <- data.frame("compound" = "N/A", 
+#                         "kinase" = "N/A", 
+#                         "percent_activity" = "N/A", 
+#                         "percent_offtarget" = "N/A")
+#   } else {
+#     query
+#   }
+# 
+# }
